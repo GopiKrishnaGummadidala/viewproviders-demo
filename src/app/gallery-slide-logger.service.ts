c87fa331425d12619d2a55436673cd4ab7e9196d
Gopi <gopikrishna.gummadidala@gmail.com>
2021-04-25 19:34:46 +0200

    latest code changes

--- src/app/gallery-slide-logger.service.ts
+++ src/app/gallery-slide-logger.service.ts
@@ -4,10 +4,9 @@ import { Injectable } from "@angular/core";
 @Injectable({
   providedIn: "root",
 })
-export class GalleryLoggerService implements ILogger {
-  private _prefix = "Gallery Logger";
+export class GallerySlideLoggerService implements ILogger {
+  private _prefix = "Gallery Slide Logger";
   constructor() {}
-
   logMessage(msg: string): void {
     console.log(`${this._prefix} : ${msg}`);
   }
