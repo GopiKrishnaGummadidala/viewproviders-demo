6557cd88415f80c81e904815a03c7cbcb40609a7
Gopi <gopikrishna.gummadidala@gmail.com>
2021-04-25 17:56:47 +0200

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { GallerySlideComponent } from './gallery-slide/gallery-slide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    GallerySlideComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/gallery-slide/gallery-slide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-slide',
+  templateUrl: './gallery-slide.component.html',
+  styleUrls: ['./gallery-slide.component.scss']
+})
+export class GallerySlideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
